ae54abe92683a101d2a609b89b79a155c12b6037
Genesispro-maker <genesisprog894@gmail.com>
2026-02-01 04:11:56 -0800

    Delete formatTime.js

--- dist/formatTime.js
+++
@@ -1,9 +0,0 @@
-export function formatTime(time) {
-    if (isNaN(time))
-        return;
-    const abs = Math.abs(time);
-    const min = Math.floor(abs / 60);
-    const sec = Math.floor(abs % 60);
-    return `${min}:${sec}`;
-}
-//# sourceMappingURL=formatTime.js.map
