e06cc981d585bc9b57ffab603349aabcef41c1fd
Genesispro-maker <genesisprog894@gmail.com>
2026-02-01 04:11:23 -0800

    Update audio.js

--- dist/audio.js
+++ dist/audio.js
@@ -1,4 +1,11 @@
-import { formatTime } from "./formatTime";
+function formatTime(time) {
+    if (isNaN(time))
+        return "0.00";
+    const abs = Math.abs(time);
+    const min = Math.floor(abs / 60);
+    const sec = Math.floor(abs % 60);
+    return `${min}:${sec}`;
+}
 export default class Noise {
     audioContext;
     gainNode;
@@ -7,19 +14,21 @@ export default class Noise {
     loop;
     audio;
     duration;
+    currentTime;
     constructor({ src, volume = 1, pan = 0, loop = false }) {
         this.audioContext = new AudioContext();
         this.audio = new Audio(src);
         this.loop = this.audio.loop = loop;
-        this.duration = this.audio.duration;
+        this.duration = formatTime(this.audio.duration);
+        this.currentTime = formatTime(this.audio.currentTime);
         this.Source = this.audioContext.createMediaElementSource(this.audio);
         this.gainNode = this.audioContext.createGain();
         this.panner = new StereoPannerNode(this.audioContext);
-        this.audio.addEventListener("loadeddata", () => {
-            this.duration = this.audio.duration;
-        });
         this.gainNode.gain.value = volume;
         this.panner.pan.value = pan;
+        this.audio.addEventListener("loadedmetadata", () => {
+            return this.duration = formatTime(this.audio.duration);
+        });
         this.Source.connect(this.panner).connect(this.gainNode).connect(this.audioContext.destination);
     }
     async play() {
@@ -30,4 +39,14 @@ export default class Noise {
         this.audio.pause();
     }
 }
+const noise = new Noise({
+    src: "/plenty.mp3",
+});
+const play = document.querySelector(".play");
+const h1 = document.createElement("h1");
+h1.textContent = `${noise.duration}`;
+document.body.appendChild(h1);
+play.addEventListener("click", () => {
+    noise.play();
+});
 //# sourceMappingURL=audio.js.map
