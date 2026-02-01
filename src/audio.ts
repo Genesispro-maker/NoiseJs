5bf5874ce2697a5a5d144f5a98e1dd4e531f2a5e
Genesispro-maker <genesisprog894@gmail.com>
2026-02-01 04:11:49 -0800

    Update audio.ts

--- src/audio.ts
+++ src/audio.ts
@@ -1,4 +1,20 @@
-import { formatTime } from "./formatTime"
+type NoiseType = {
+    src: string,
+    volume: number,
+    pan: number,
+    loop: boolean,
+}
+
+function formatTime(time: number) {
+  if (isNaN(time)) return "0.00";
+
+  const abs = Math.abs(time);
+
+  const min = Math.floor(abs / 60);
+  const sec = Math.floor(abs % 60);
+
+  return `${min}:${sec}`
+}
 
 export default class Noise{
     private audioContext : AudioContext
@@ -7,23 +23,24 @@ export default class Noise{
     private Source : MediaElementAudioSourceNode
     private loop : boolean
     private audio: HTMLAudioElement
-    public duration: number
-    constructor({src, volume = 1, pan = 0, loop = false}: {src: string, volume: number, pan: number, loop: boolean}){
+    public duration: string
+    public currentTime: string | undefined
+
+    constructor({src, volume = 1, pan = 0, loop = false}: Partial<NoiseType>){
         this.audioContext = new AudioContext()
         this.audio = new Audio(src)
         this.loop = this.audio.loop = loop
-        this.duration = this.audio.duration
+        this.duration = formatTime(this.audio.duration)
+        this.currentTime = formatTime(this.audio.currentTime)
         this.Source = this.audioContext.createMediaElementSource(this.audio)
         this.gainNode = this.audioContext.createGain()
         this.panner = new StereoPannerNode(this.audioContext)
-
-        this.audio.addEventListener("loadeddata", () => {
-            this.duration = this.audio.duration;
-        })
-
-
         this.gainNode.gain.value =  volume
         this.panner.pan.value = pan
+
+        this.audio.addEventListener("loadedmetadata", () => {
+            return this.duration = this.audio.duration
+        })
 
         this.Source.connect(this.panner).connect(this.gainNode).connect(this.audioContext.destination)
     }
@@ -39,3 +56,17 @@ export default class Noise{
         this.audio.pause()
     }
 }
+
+const noise = new Noise({
+    src: "/plenty.mp3",
+})
+
+const play = document.querySelector(".play") as HTMLElement
+const h1 = document.createElement("h1")
+h1.textContent = `${noise.duration}`
+document.body.appendChild(h1)
+
+
+play.addEventListener("click", () => {
+  noise.play()
+})
